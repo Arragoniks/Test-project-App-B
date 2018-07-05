b1975c1cbc4e81fc754d8affd38aaa6415782184
Roman <mailmyworkmail@gmail.com>
2018-07-05 18:47:14 +0300

    Main update and Manifest

--- app/src/main/java/com/example/komputer/app_b/MainActivity.java
+++ app/src/main/java/com/example/komputer/app_b/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.komputer.app_b;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    //String url = "http://www.radionetplus.ru/uploads/posts/2013-05/1369460621_panda-26.jpg";
+    //ImageView imgv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //imgv = findViewById(R.id.image);
+        //ImageHTTPRequest imgh = new ImageHTTPRequest(imgv);
+        //imgh.execute(url);
+        //ImageHTTPDownload imgs = new ImageHTTPDownload();
+        //imgs.downloadImg(url, this);
+
     }
 }
