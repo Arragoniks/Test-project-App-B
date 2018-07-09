731d8ee5c37999e1f138f0d2d890eb66d503e192
Mykyta-Novikov <38505408+Mykyta-Novikov@users.noreply.github.com>
2018-07-09 15:47:51 +0300

    Fixed problem with statuses.

--- app/src/main/java/com/example/komputer/app_b/MainActivity.java
+++ app/src/main/java/com/example/komputer/app_b/MainActivity.java
@@ -19,6 +19,7 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
 
+@SuppressWarnings("FieldCanBeLocal")
 public class MainActivity extends AppCompatActivity {
     private static final int PERMISSIONS_GET = 1;
     private static final String[] PERMISSIONS = {Manifest.permission.INTERNET, Manifest.permission.READ_EXTERNAL_STORAGE,
@@ -71,7 +72,7 @@ public class MainActivity extends AppCompatActivity {
                 }
             };
             countDownTimer.start();
-        }else if(isAPicture()){
+        }else if(!isAPicture()){
             if(openHist)
                 dbAccessHelper.updateStatus(url, 2);
             else
